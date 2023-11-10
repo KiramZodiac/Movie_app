f63b2ddae61a9ea6f4deb2ec5d6760cc3f0dd5ff
KiramZodiac <kiramzodiac@gmail.com>
2023-11-10 11:58:15 +0300

    my movie app on testing api

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ const App = () => {
   const movieList = movies.map((movie) => {
     return (
       <div className="movies">
-        <h1>Kiram movie app</h1>
         <img height="200px" width="200px" src={movie.Poster} alt="img" />
         <h1>{movie.Title}</h1>
         <h2>{movie.Year}</h2>
@@ -21,7 +20,6 @@ const App = () => {
   });
   const getMovies = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=afafbc0e`;
-
     const res = await fetch(url);
     const resJson = await res.json();
     if (resJson.Search) {
@@ -35,6 +33,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1>Kiram movie app</h1>
       <h1>hello movies</h1>
       <SearchBox searchValue={searchValue} setSearch={setSearchValue} />
       {movieList}
